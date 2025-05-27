53649b215d9e81fab1a514536ba27c732cc71eac
Kiyohiro Kambayashi <kiyohiro.0310@gmail.com>
2025-05-27 20:55:25 +1000

    All fucntions have been implemented

--- components/fragments/file/Uploader.tsx
+++ components/fragments/file/Uploader.tsx
@@ -41,7 +41,7 @@ const Uploader = ({
           <button
             type="button"
             onClick={() => open()}
-            className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Upload Image
           </button>
